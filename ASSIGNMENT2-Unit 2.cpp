ec1632a97c3940e9323657ebe58b5d60e6f27245
Shantanu-2107 <shantanusolegaonkar21@gmail.com>
2026-09-08 14:51:14 +0530

    Add files via upload

---
+++ ASSIGNMENT2-Unit 2.cpp
@@ -0,0 +1,52 @@
+
+#include<iostream>
+using namespace std;
+class student
+{
+    public:
+    int id;
+    string name;
+    float marks;
+    student()
+    {
+       name="";
+       id=0;
+       marks=0;
+
+    }
+
+    student(int id,string name, float marks)
+    {
+        this->id = id;
+        this->name = name;
+        this->marks = marks;
+    }
+    void display()
+    {
+        cout<<"---------Student details-----------"<<endl;
+        cout<<"Student id"<<this->id<<endl;
+        cout<<"Student name"<<this->name<<endl;
+        cout<<"Student marks"<<this->marks<<endl;
+        
+    }
+    void input()
+    {
+        cout<<"Enter Student id - ";
+        cin>>this->id;
+
+        cout<<"Enter Student name - ";
+        cin>>this->name;
+
+        cout<<"Enter Student marks - ";
+        cin>>this->marks;
+    }
+    };
+    int main()
+    {
+        student s1;
+        s1.input();
+        s1.display();
+        return 0;
+    };
+
+
